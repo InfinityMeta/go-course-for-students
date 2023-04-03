56761c021e4d4a7901b68d7fa2bed110756f9dac
InfinityMeta <maksim.golovin.2001@mail.ru>
2023-04-03 22:08:25 +0300

    hw5 main trans

--- lesson5/homework/pipeline.go
+++ lesson5/homework/pipeline.go
@@ -11,42 +11,7 @@ type (
 
 type Stage func(in In) (out Out)
 
-func h(ctx context.Context) Stage {
-	return func(in In) Out {
-		out := make(chan any)
-
-		go func(out chan any) {
-			defer close(out)
-			for range ctx.Done() {
-			}
-		}(out)
-
-		go func(in In, out chan any) {
-			defer close(out)
-			for {
-				v, ok := <-in
-				if !ok {
-					return
-				}
-				out <- v
-			}
-		}(in, out)
-		return out
-	}
-}
-
 func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
-
-	stages = append(stages, h(ctx))
-
-	stagesRes := make([]Out, len(stages)+1)
-
-	stagesRes[0] = in
-
-	for i, st := range stages {
-		stagesRes[i+1] = st(stagesRes[i])
-	}
-
-	return stagesRes[len(stagesRes)-1]
-
+	// TODO
+	return nil
 }
