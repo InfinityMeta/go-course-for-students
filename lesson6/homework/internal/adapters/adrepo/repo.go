f117e49175e2b7c1388654ee69be0b4b6e1a6e89
InfinityMeta <maksim.golovin.2001@mail.ru>
2023-04-18 12:50:08 +0300

    hw6

--- lesson6/homework/internal/adapters/adrepo/repo.go
+++ lesson6/homework/internal/adapters/adrepo/repo.go
@@ -1,9 +1,50 @@
 package adrepo
 
 import (
+	"context"
+	"fmt"
+
+	"homework6/internal/ads"
 	"homework6/internal/app"
 )
 
+type RepositoryMap struct {
+	storage map[int64]*ads.Ad
+}
+
 func New() app.Repository {
-	return nil // TODO: реализовать
+	storage := make(map[int64]*ads.Ad)
+	return &RepositoryMap{storage: storage}
+}
+
+func (rs *RepositoryMap) GetAdByID(ctx context.Context, id int64) (*ads.Ad, error) {
+
+	var err error
+
+	ad, ok := rs.storage[id]
+
+	if !ok {
+		err = fmt.Errorf("ad with id %d not found", id)
+		return &ads.Ad{}, err
+	}
+
+	return ad, err
+
+}
+
+func (rs *RepositoryMap) StoreAd(ctx context.Context, ad *ads.Ad) error {
+
+	var err error
+
+	_, ok := rs.storage[ad.ID]
+
+	if ok {
+		err = fmt.Errorf("ad with id %d already exists", ad.ID)
+		return err
+	}
+
+	rs.storage[ad.ID] = ad
+
+	return nil
+
 }
--- lesson6/homework/internal/app/app.go
+++ lesson6/homework/internal/app/app.go
@@ -1,13 +1,84 @@
 package app
 
+import (
+	"context"
+	"errors"
+
+	"homework6/internal/ads"
+)
+
 type App interface {
 	// TODO: реализовать
+	CreateAd(context.Context, string, string, int64) (*ads.Ad, error)
+	ChangeAdStatus(context.Context, int64, int64, bool) (*ads.Ad, error)
+	UpdateAd(context.Context, int64, int64, string, string) (*ads.Ad, error)
 }
 
 type Repository interface {
 	// TODO: реализовать
+	StoreAd(context.Context, *ads.Ad) error
+	GetAdByID(context.Context, int64) (*ads.Ad, error)
+}
+
+type AdApp struct {
+	repository Repository
+	adCount    int64
 }
 
 func NewApp(repo Repository) App {
-	return nil // TODO: реализовать
+	return &AdApp{repository: repo}
+}
+
+func (a *AdApp) CreateAd(ctx context.Context, title string, text string, authorId int64) (*ads.Ad, error) {
+
+	ad := &ads.Ad{ID: a.adCount, Title: title, Text: text, AuthorID: authorId, Published: false}
+	a.adCount++
+
+	err := a.repository.StoreAd(ctx, ad)
+
+	if err != nil {
+		return &ads.Ad{}, err
+	}
+
+	return ad, nil
+
+}
+
+func (a *AdApp) ChangeAdStatus(ctx context.Context, adId int64, authorId int64, published bool) (*ads.Ad, error) {
+
+	ad, err := a.repository.GetAdByID(ctx, adId)
+
+	if err != nil {
+		return &ads.Ad{}, err
+	}
+
+	if ad.AuthorID != authorId {
+		err = errors.New("status forbidden")
+		return &ads.Ad{}, err
+	}
+
+	ad.Published = published
+
+	return ad, nil
+
+}
+
+func (a *AdApp) UpdateAd(ctx context.Context, adId int64, userId int64, title string, text string) (*ads.Ad, error) {
+
+	ad, err := a.repository.GetAdByID(ctx, adId)
+
+	if err != nil {
+		return &ads.Ad{}, err
+	}
+
+	if ad.AuthorID != userId {
+		err = errors.New("status forbidden")
+		return &ads.Ad{}, err
+	}
+
+	ad.Title = title
+	ad.Text = text
+
+	return ad, nil
+
 }
